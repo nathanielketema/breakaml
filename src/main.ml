58bf61d8bb161893cef2ec94281bd68922df8c32
Nathaniel Ketema <nathanielketema@gmail.com>
2026-06-05 10:30:24 -0500

    raylib window

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,16 @@
-let () = print_endline "Hello world!"
+let setup () =
+  Raylib.init_window 800 450 "raylib [core] example - basic window";
+  Raylib.set_target_fps 60
+
+let loop () =
+  let open Raylib in
+  while not (window_should_close ()) do
+    begin_drawing ();
+    clear_background Color.raywhite;
+    draw_text "Congrats! You created your first window!" 190 200 20
+      Color.lightgray;
+    end_drawing ()
+  done;
+  close_window ()
+
+let () = setup () |> loop
