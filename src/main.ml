009b60bbb5fb5d5a41858cde645ca9f52b622d07
Nathaniel Ketema <nathanielketema@gmail.com>
2026-06-05 17:46:44 -0500

    moving rectangle

--- src/main.ml
+++ src/main.ml
@@ -1,15 +1,20 @@
+open Raylib
+
 let setup () =
   Raylib.init_window 700 450 "Breakaml";
   Raylib.set_target_fps 60
 
-let loop () =
-  let open Raylib in
+let game_loop () =
+  let x = ref 10 in
+  let y = ref 10 in
   while not (window_should_close ()) do
     begin_drawing ();
     clear_background Color.raywhite;
-    draw_rectangle 300 150 100 100 Color.red;
+    draw_rectangle !x !y 100 100 Color.red;
+    x := !x + 1;
+    incr y;
     end_drawing ()
   done;
   close_window ()
 
-let () = setup () |> loop
+let () = setup () |> game_loop
