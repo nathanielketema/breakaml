40c8e06096e2a5516083e4bf4880784cefae9463
Nathaniel Ketema <nathanielketema@gmail.com>
2026-06-05 11:33:07 -0500

    red rectangle in the middle....let's go

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,5 @@
 let setup () =
-  Raylib.init_window 800 450 "raylib [core] example - basic window";
+  Raylib.init_window 700 450 "Breakaml";
   Raylib.set_target_fps 60
 
 let loop () =
@@ -7,8 +7,7 @@ let loop () =
   while not (window_should_close ()) do
     begin_drawing ();
     clear_background Color.raywhite;
-    draw_text "Congrats! You created your first window!" 190 200 20
-      Color.lightgray;
+    draw_rectangle 300 150 100 100 Color.red;
     end_drawing ()
   done;
   close_window ()
